eaf95831760b00615b8397f14d733752eaa9dca8
RONALDO UCHOA <ronaldo.uchoa.pessoal@gmail.com>
2023-08-29 16:10:47 -0300

    env: apiKey Update Security

--- src/app/components/weather-api.service.ts
+++ src/app/components/weather-api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Weather } from './weather';
-import { environment } from 'src/environments/enviroment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/enviroment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiKey: '449d4163671c1487c4729cc196a2149f'
-}
