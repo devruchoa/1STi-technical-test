7d5670bb487f8e0d0b9989742c0869efafbd49e0
RONALDO UCHOA <ronaldo.uchoa.pessoal@gmail.com>
2023-08-29 19:15:12 -0300

    env: test deploy

--- src/app/components/weather-api.service.ts
+++ src/app/components/weather-api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Weather } from './weather';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
